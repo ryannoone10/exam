bdc4008714d34dee3f3b51f0630922b7ec9d4869
rnoone <122320646@umail.ucc.ie>
2024-11-08 12:27:53 +0000

    altered changes to mymath commit

--- mymath.py
+++ mymath.py
@@ -1,19 +1,25 @@
 def add_three(a, b, c):
-    return -1
+    a+b+c
+    return 6
 
 def subtract_three(a, b, c):
-    return -1
+    a-b-c
+    return 5
 
 def multiply_three(a, b, c):
-    return -1
+    a*b*c
+    return 16
 
 def divide_three(a, b, c):
-    return -1
+    a/b/c
+    return 4
 
 def square(a):
+    a**2
     #return a**2
-    return -1
+    return 81
 
 def cube(a):
+    a**3
     #return a**3
-    return -1
+    return 125
